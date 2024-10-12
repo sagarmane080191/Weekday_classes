7eb7986c76f58a14a556b2a7eb39641c15197f0e
Admin <Admin@DESKTOP-AAHQR57>
2024-10-12 20:47:12 +0530

    First demo

---
+++ Oct_10/src/test/Demo.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String name= "Sagar";
+		System.out.println(name);
+		
+		double DOB= 8011991;
+		System.out.println(DOB);
+		// TODO Auto-generated method stub
+
+	}
+
+}
