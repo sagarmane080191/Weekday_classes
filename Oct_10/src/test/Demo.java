8ba573ef664c68ae90486b6896ea643479fce515
Admin <Admin@DESKTOP-AAHQR57>
2024-10-13 22:47:05 +0530

    update

--- Oct_10/src/test/Demo.java
+++ Oct_10/src/test/Demo.java
@@ -1,27 +1,57 @@
 package test;
 
+import java.util.Scanner;
+
 public class Demo {
 
 	private static final boolean True = false;
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int size = sc.nextInt();
 		
-		String name= "Sagar";
-		System.out.println(name);
+//		String name= "Sagar";
+//		System.out.println(name);
+//		
+//		double DOB= 8011991;
+//		System.out.println(DOB);
+//		
+//		float pi= (float) 3555555.144454;
+//		System.out.println(pi);
+//		// TODO Auto-generated method stub
+//		
+//		boolean isName= True;
+//		System.out.println(isName);
+//		
+//		int N= 8;
+//		System.out.println(N);
+//		
+		//Array
+		int arr[] = new int[size];
+		//arr[0]= 55;
+		//arr[1]= 66;
+		//arr[2]= 77;
+		//arr[3]= 88;
+		//System.out.println(arr[0]);
+		//System.out.println(arr[1]);
 		
-		double DOB= 8011991;
-		System.out.println(DOB);
+		// this for loop is for the input
+		for(int i=0; i<size; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
 		
-		float pi= (float) 3555555.144454;
-		System.out.println(pi);
-		// TODO Auto-generated method stub
+		int x = sc.nextInt();
 		
-		boolean isName= True;
-		System.out.println(isName);
-		
-		int N= 8;
-		System.out.println(N);
-		
+		// this for loop is for the output
+		for (int i=0; i<arr.length; i++)
+		{
+			if (arr[i]==x)
+			{				
+			System.out.println("x found at index:" +i);
+			}
+		}
+			
 		
 
 		
